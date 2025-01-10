be4d84b22c10993a186b6671e753b267a1cc28f8
Ravinarayana U <ravinarayanau2@gmail.com>
2025-01-10 16:59:21 +0530

    added cards

---
+++ site/components/Display.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from "./FoodCard";
+
+const Display = () => {
+  return (
+    <div className='p-10 grid-rows-*'>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+    </div>
+  )
+}
+
+export default Display
