720db433db765f79042ed57d953731304c5834fe
loystonpais <loyston500@gmail.com>
2025-01-10 13:09:39 +0530

    temporary android studio setup build succeeds. android app runs  *

--- shell.nix
+++ shell.nix
@@ -6,5 +6,19 @@ pkgs.mkShell {
     yarn
     yarn2nix
     prefetch-npm-deps
+    android-studio
   ];
+
+
+  # DOC
+  # the android release key is under android folder.
+  # To build the app v
+  #! npx cap build android --keystorepath releasekey.keystore --keystorepass 123456 --keystorealias alias_name --keystorealiaspass 123456 --androidreleasetype APK
+
+  # Installing the app.
+  #! npx cap run android
+
+  # temp android studio setup
+  CAPACITOR_ANDROID_STUDIO_PATH = "${pkgs.android-studio}/bin/android-studio";
+  ANDROID_HOME = "/home/loystonpais/Android/Sdk";
 }
